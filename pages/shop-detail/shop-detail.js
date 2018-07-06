b658d11c8080d261c2cc6c460b1bdc19faaae5b8
youhuahaohaoshuo <591005835@qq.com>
2018-07-07 00:14:41 +0800

    feat:new page

--- pages/shop-detail/shop-detail.js
+++ pages/shop-detail/shop-detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    
   },
 
   /**
@@ -15,52 +15,19 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+  joinCart () {
+    wx.showLoading({
+      title: '正在加入购物车',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    setTimeout(function () {
+      wx.hideLoading()
+      wx.showToast({
+        title: '加入成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }, 2000)
   }
 })
--- pages/shoppingcart/shoppingcart.js
+++ pages/shoppingcart/shoppingcart.js
@@ -3,11 +3,81 @@ Page({
 
   data: {
     items: [
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-    ]
+      { name: 'USA', value: '美国', price: 30, number: 3 },
+      { name: 'CHN', value: '中国', price: 50.7, number: 6 }
+    ],
+    shoppingNumber: 0,
+    countPrice: 0,
+    isSelectAll: false,
+    orderList: []
   },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  checkboxChange (e) {
+    this.setData({
+      orderList: e.detail.value
+    })
+    if (e.detail.value.length === this.data.items.length) {
+      this.setData({
+        isSelectAll: true
+      })
+    } else {
+      this.setData({
+        isSelectAll: false
+      })
+    }
+    this.countNumberAndPrice()
+  },
+  selectAll () {
+    this.setData({
+      isSelectAll: !this.data.isSelectAll
+    })
+    let items = this.data.items
+    if (this.data.isSelectAll) {
+      let orderList = []
+      items.forEach(item => {
+        item.checked = true
+        orderList.push(item.name)
+      })
+      this.setData({
+        orderList
+      })
+      this.countNumberAndPrice()
+    } else {
+      items.forEach(item => {
+        item.checked = false
+      })
+      this.setData({
+        shoppingNumber: 0,
+        countPrice: 0,
+        orderList: []
+      })
+    }
+    this.setData({
+      items
+    })
+  },
+  countNumberAndPrice () {
+    let shoppingNumber = 0
+    let countPrice = 0
+    this.data.orderList.forEach(selectItem => {
+      let items = this.data.items.find(item => {
+        return item.name === selectItem
+      })
+      console.log(items)
+      shoppingNumber += items.number * 1
+      countPrice += items.price * 1
+    })
+    this.setData({
+      shoppingNumber,
+      countPrice
+    })
+  },
+  balanceNow () {
+    if (this.data.orderList.length === 0) {
+      wx.showToast({
+        title: '请选择结算的商品',
+        icon: 'none',
+        duration: 1000
+      })
+    }
   }
 })
